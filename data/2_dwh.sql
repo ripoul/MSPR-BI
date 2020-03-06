82859441bf519fff1ee89cc694e3484b0b387327
Lucas Martineau <lucas.martineau@sharingcloud.com>
2020-03-06 15:40:24 +0100

    :sparkles: refacto dwh schema

--- data/2_dwh.sql
+++ data/2_dwh.sql
@@ -8,25 +8,15 @@ CREATE TABLE IF NOT EXISTS dwh."day" (
 
 CREATE TABLE IF NOT EXISTS dwh."month" (
   "id" SERIAL PRIMARY KEY,
+  "day_id" int,
   "num_month" int,
   "month_name" varchar
 );
 
 CREATE TABLE IF NOT EXISTS dwh."year" (
   "id" SERIAL PRIMARY KEY,
-  "num_year" int
-);
-
-CREATE TABLE IF NOT EXISTS dwh."time" (
-  "id" SERIAL PRIMARY KEY,
-  "year_id" int,
-  "month_id" int,
-  "day_id" int
-);
-
-CREATE TABLE IF NOT EXISTS dwh."city" (
-  "id" int PRIMARY KEY,
-  "name" varchar
+  "num_year" int,
+  "month_id" int
 );
 
 CREATE TABLE IF NOT EXISTS dwh."client" (
@@ -42,20 +32,15 @@ CREATE TABLE IF NOT EXISTS dwh."supplier" (
 );
 
 CREATE TABLE IF NOT EXISTS dwh."product_category" (
-  "id" int PRIMARY KEY,
-  "name" varchar
-);
-
-CREATE TABLE IF NOT EXISTS dwh."material" (
-  "id" int PRIMARY KEY,
-  "name" varchar
+  "id" SERIAL PRIMARY KEY,
+  "name" varchar,
+  "material_id" int
 );
 
 CREATE TABLE IF NOT EXISTS dwh."product" (
   "id" SERIAL PRIMARY KEY,
   "label" varchar,
-  "category_id" int,
-  "material_id" int
+  "category_id" int
 );
 
 CREATE TABLE IF NOT EXISTS dwh."site" (
@@ -64,51 +49,58 @@ CREATE TABLE IF NOT EXISTS dwh."site" (
 );
 
 CREATE TABLE IF NOT EXISTS dwh."sale" (
-  "id" int PRIMARY KEY,
+  "id" SERIAL PRIMARY KEY,
   "client_id" int,
   "site_id" int,
-  "time_id" int,
+  "year_id" int,
   "product_id" int,
   "quantity" int,
   "revenue" float
 );
 
 CREATE TABLE IF NOT EXISTS dwh."purchase" (
-  "id" int PRIMARY KEY,
+  "id" SERIAL PRIMARY KEY,
   "supplier_id" int,
   "site_id" int,
-  "time_id" int,
+  "year_id" int,
   "product_id" int,
   "quantity" int,
   "revenue" float
 );
 
-ALTER TABLE dwh."time" ADD FOREIGN KEY ("year_id") REFERENCES dwh."year" ("id");
+CREATE TABLE IF NOT EXISTS dwh."stock" (
+  "id" SERIAL PRIMARY KEY,
+  "quantity" int,
+  "value" float,
+  "year_id" int,
+  "product_id" int,
+  "site_id" int
+);
 
-ALTER TABLE dwh."time" ADD FOREIGN KEY ("month_id") REFERENCES dwh."month" ("id");
+ALTER TABLE dwh."year" ADD FOREIGN KEY ("month_id") REFERENCES dwh."month" ("id");
 
-ALTER TABLE dwh."time" ADD FOREIGN KEY ("day_id") REFERENCES dwh."day" ("id");
-
-ALTER TABLE dwh."client" ADD FOREIGN KEY ("city_id") REFERENCES dwh."city" ("id");
-
-ALTER TABLE dwh."supplier" ADD FOREIGN KEY ("city_id") REFERENCES dwh."city" ("id");
+ALTER TABLE dwh."month" ADD FOREIGN KEY ("day_id") REFERENCES dwh."day" ("id");
 
 ALTER TABLE dwh."product" ADD FOREIGN KEY ("category_id") REFERENCES dwh."product_category" ("id");
-
-ALTER TABLE dwh."product" ADD FOREIGN KEY ("material_id") REFERENCES dwh."material" ("id");
 
 ALTER TABLE dwh."sale" ADD FOREIGN KEY ("client_id") REFERENCES dwh."client" ("id");
 
 ALTER TABLE dwh."sale" ADD FOREIGN KEY ("site_id") REFERENCES dwh."site" ("id");
 
-ALTER TABLE dwh."sale" ADD FOREIGN KEY ("time_id") REFERENCES dwh."time" ("id");
-
 ALTER TABLE dwh."sale" ADD FOREIGN KEY ("product_id") REFERENCES dwh."product" ("id");
+
+ALTER TABLE dwh."sale" ADD FOREIGN KEY ("year_id") REFERENCES dwh."year" ("id");
 
 ALTER TABLE dwh."purchase" ADD FOREIGN KEY ("supplier_id") REFERENCES dwh."supplier" ("id");
 
 ALTER TABLE dwh."purchase" ADD FOREIGN KEY ("site_id") REFERENCES dwh."site" ("id");
 
-ALTER TABLE dwh."purchase" ADD FOREIGN KEY ("time_id") REFERENCES dwh."time" ("id");
+ALTER TABLE dwh."purchase" ADD FOREIGN KEY ("year_id") REFERENCES dwh."year" ("id");
 
 ALTER TABLE dwh."purchase" ADD FOREIGN KEY ("product_id") REFERENCES dwh."product" ("id");
+
+ALTER TABLE dwh."stock" ADD FOREIGN KEY ("year_id") REFERENCES dwh."year" ("id");
+
+ALTER TABLE dwh."stock" ADD FOREIGN KEY ("product_id") REFERENCES dwh."product" ("id");
+
+ALTER TABLE dwh."stock" ADD FOREIGN KEY ("site_id") REFERENCES dwh."site" ("id");
